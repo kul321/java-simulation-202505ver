b387a0f0d8daef35cf70ebb379042aafa88cfd50
KangMinseo <kul321@naver.com>
2025-05-31 08:26:39 +0900

    automode

--- CoffeeShop/Main.java
+++ CoffeeShop/Main.java
@@ -1,10 +1,14 @@
 package project.CoffeeShop;
 
 public class Main {
+    public static void main(String[] args) {
+        ShopManager shopManager = new ShopManager();
 
-    public static void main(String[] args){
-    ShopManager shopManager = new ShopManager();
-
-     shopManager.start();
+        // 자동모드 실행 (인자가 "auto"일 때)
+        if (args.length > 0 && args[0].equals("auto")) {
+            shopManager.runAutoMode(); //
+        } else {
+            shopManager.start(); // 기존 CLI 메뉴
+        }
     }
 }
